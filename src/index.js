0f8749f2e52fb06a587a957cd1e57eed22baa67a
cromewar <vasiliy@cromewar.com>
2021-11-09 15:00:19 -0500

    basic Moralis and Header/Main

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <MoralisProvider
+    appId={process.env.REACT_APP_MORALIS_APP_ID}
+    serverUrl={process.env.REACT_APP_MORALIS_SERVER_URL}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </MoralisProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
